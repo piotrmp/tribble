45262c573d6e9e3d8a88e95c2344a8c57f4514d1
piotrmp <piotr.m.przybyla@gmail.com>
2024-05-28 12:57:14 +0200

    Added sentence splitting.

---
+++ harvesting/split_sentences.py
@@ -0,0 +1,20 @@
+import pathlib
+from lambo.segmenter.lambo import Lambo
+
+input_file = pathlib.Path.home() / 'data' / 'tribble' / 'an_text.tsv'
+output_file = pathlib.Path.home() / 'data' / 'tribble' / 'an_text_sentences.tsv'
+
+lambo = Lambo.get('Spanish')
+handle = open(output_file,'w')
+
+for line in open(input_file):
+    parts = line.split('\t')
+    print(parts[0])
+    text = parts[2].replace('\\n','\n').replace('\\t','\t')
+    document = lambo.segment(text)
+    for turn in document.turns:
+        for sentence in turn.sentences:
+            handle.write(parts[0]+'\t'+parts[1]+'\t'+sentence.text.strip().replace('\t','\\t').replace('\n','\\n')+'\n')
+    #break
+
+handle.close()
