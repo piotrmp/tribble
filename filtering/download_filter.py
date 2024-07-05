b0a572c66cc53e0f3645e362b886e0b737719ff3
piotrmp <piotr.m.przybyla@gmail.com>
2024-07-05 13:01:04 +0200

    Small tweaks.

--- filtering/download_filter.py
+++ filtering/download_filter.py
@@ -93,7 +93,7 @@ for target_lang in ['an', 'oc', 'ast']:
     print(available_corpora)
     for corpus in available_corpora:
         print("CORPUS: " + corpus)
-        if target_lang == 'oc' and corpus == 'CCMatrix':
+        if target_lang == 'oc' and corpus in ['CCMatrix','KDE4']:
             # Causing error in OPUS reader
             print("BLACKLISTED, SKIPPING")
             continue
